ebe6c12cc4324dc2c3666fbc9fa8a2da0fcb40a9
momo-87 <momochristian87@gmail.com>
2024-01-06 06:59:26 +0100

    Implement Model testing

--- app/models/query_analytic.rb
+++ app/models/query_analytic.rb
@@ -1,2 +1,5 @@
 class QueryAnalytic < ApplicationRecord
+  validates :query, presence: true, length: { maximum: 250 }
+  validates :ip_address, presence: true
+  validates :complete, presence: true
 end
